5f6067444944ac8b785c28075070a8103a0cf1f1
martas <mk.sroczynska@gmail.com>
2018-07-27 12:07:09 +0200

    Empty main

--- main.cpp
+++ main.cpp
@@ -8,13 +8,5 @@
 using namespace std;
 
 int main() {
-    std::string testing_data_dir = "//home//martas//Dropbox//Doktoratowe//AIcollisions//MultichannelNumerovAIWithTests_1//MultichannelNumerovAIWithTests//TestData//";
 
-    std::vector<std::string> filenamesList = {
-            testing_data_dir + "good_file_with_params.txt",
-            testing_data_dir + "good_EList_doubles.dat",
-            testing_data_dir + "good_V_two_channels_nx=3.txt",
-            testing_data_dir + "good_B_two_channels"
-    };
-    Parameters p(filenamesList);
 }
